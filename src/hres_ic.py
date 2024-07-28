32d8cf5e2e519ec3e9a7c2ee226341a814f199f1
Davide Marchegiani <davide.marchegiani@gmail.com>
2024-07-29 09:09:40 +1000

    Added 'hres_eccb.py' and 'hres_ic.py' to the repo.

---
+++ src/hres_ic.py
@@ -0,0 +1,63 @@
+#!/g/data/hh5/public/apps/miniconda3/envs/analysis3-23.01/bin/python
+
+"""
+Replace the land/surface fields in the astart file with higher-resolution
+era5-land or BARRA2-R data (if requested).
+"""
+
+from pathlib import Path
+import argparse
+from datetime import datetime,timedelta
+import pandas
+import shutil
+
+import replace_landsurface_with_ERA5land_IC 
+import replace_landsurface_with_BARRA2R_IC
+
+boolopt = {
+    "True": True,
+    "False": False,
+}
+
+
+def main():
+
+    """
+    The main function that creates a worker pool and generates single GRIB files 
+    for requested date/times in parallel.
+
+    Parameters
+    ----------
+    None.  The arguments are given via the command-line
+
+    Returns
+    -------
+    None.  The astart file is updated and overwritten
+    """ 
+
+    # Parse the command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mask', required=True, type=Path)
+    parser.add_argument('--file', required=True, type=Path)
+    parser.add_argument('--start', required=True, type=pandas.to_datetime)
+    parser.add_argument('--type', default="era5land")
+    args = parser.parse_args()
+    print(args)
+
+    # Convert the date/time to a formatted string
+    t = args.start.strftime("%Y%m%dT%H%MZ")
+    print(args.mask, args.file, t)
+
+    # If necessary replace ERA5 land/surface fields with higher-resolution options
+    if "era5land" in args.type:
+        replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.mask, args.file, t)
+        shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
+    elif "barra" in args.type:
+        replace_landsurface_with_BARRA2R_IC.swap_land_barra(args.mask, args.file, t)
+        shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
+    else:
+        print("No need to swap out IC")
+
+if __name__ == '__main__':
+    main()
+
