9c1e00b126963421a2b20e5da6588bc94fc90c86
engelca <148014330+engelca@users.noreply.github.com>
2024-08-06 09:08:29 +1000

    Update common_mule_operator.py
    Whitespace

--- src/common_mule_operator.py
+++ src/common_mule_operator.py
@@ -1,12 +1,15 @@
 import mule
 
 class ReplaceOperator(mule.DataOperator):
+    
     """ Mule operator for replacing the data"""
     def __init__(self):
         pass
+        
     def new_field(self, sources):
         print('new_field')
         return sources[0]
+        
     def transform(self, sources, result):
         print('transform')
         return sources[1]
