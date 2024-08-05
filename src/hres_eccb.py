239e54004ded9ba1d6a1d3c672bd890505401e6c
engelca <148014330+engelca@users.noreply.github.com>
2024-08-06 09:10:24 +1000

    Update hres_eccb.py
    Whitespace and remove printing statement.

--- src/hres_eccb.py
+++ src/hres_eccb.py
@@ -18,21 +18,20 @@ boolopt = {
     "False": False,
 }
 
-
 def main():
     """
     The main function takes a start dump and replaces the land/surface fields with those
     from either era5-land, BARRA-R2 or a fields file.
-
+    
     Parameters
     ----------
     None.  The arguments are given via the command-line
-
+    
     Returns
     -------
     None.  The ec_cb000 file is updated and overwritten
     """
-
+    
     # Parse the command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--mask", required=True, type=Path)
@@ -41,12 +40,11 @@ def main():
     parser.add_argument("--type", default="era5land")
     parser.add_argument("--hres_ic", type=Path)
     args = parser.parse_args()
-    print(args)
 
     # Convert the date/time to a formatted string
     t = args.start.strftime("%Y%m%dT%H%MZ")
     print(args.mask, args.file, t)
-
+    
     # If necessary replace ERA5 land/surface fields with higher-resolution options
     if "era5land" in args.type:
         replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.mask, args.file, t)
