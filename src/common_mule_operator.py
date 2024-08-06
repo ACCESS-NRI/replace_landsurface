bdb359004a310f8924f056d53e9ad9b957ae58ae
engelca <148014330+engelca@users.noreply.github.com>
2024-08-06 11:53:19 +1000

    Update common_mule_operator.py
    Added in docstrings

--- src/common_mule_operator.py
+++ src/common_mule_operator.py
@@ -1,12 +1,15 @@
 import mule # pylint: disable=import-error
 
 class ReplaceOperator(mule.DataOperator):
-    """ Mule operator for replacing the data"""
+    """ Mule operator for replacing the data """
     def __init__(self):
+        """Set up the replace operator"""
         pass
     def new_field(self, sources):
+        """ Print the data from the new field """
         print('new_field')
         return sources[0]
     def transform(self, sources, result):
+        """ Tranform the data """
         print('transform')
         return sources[1]
