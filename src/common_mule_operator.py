300564a86fbf3d128f16cc12098072e47d6008fd
engelca <148014330+engelca@users.noreply.github.com>
2024-08-06 11:55:49 +1000

    Update common_mule_operator.py
    Add docstrings, remove unnecessary pass statement and input

--- src/common_mule_operator.py
+++ src/common_mule_operator.py
@@ -1,15 +1,15 @@
+"""Module providing a function to replace data in a fieldsfile."""
 import mule # pylint: disable=import-error
 
 class ReplaceOperator(mule.DataOperator):
     """ Mule operator for replacing the data """
     def __init__(self):
         """Set up the replace operator"""
-        pass
     def new_field(self, sources):
         """ Print the data from the new field """
         print('new_field')
         return sources[0]
-    def transform(self, sources, result):
+    def transform(self, sources):
         """ Tranform the data """
         print('transform')
         return sources[1]
