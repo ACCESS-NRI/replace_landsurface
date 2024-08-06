c40c8a7faeac85e2dadbf45a63106bfcccec30c6
engelca <148014330+engelca@users.noreply.github.com>
2024-08-06 11:51:06 +1000

    Update common_mule_operator.py
    remove trailing whitespace

--- src/common_mule_operator.py
+++ src/common_mule_operator.py
@@ -4,11 +4,9 @@ class ReplaceOperator(mule.DataOperator):
     """ Mule operator for replacing the data"""
     def __init__(self):
         pass
-    
     def new_field(self, sources):
         print('new_field')
         return sources[0]
-        
     def transform(self, sources, result):
         print('transform')
         return sources[1]
