72d585d4e2e89dfe73b79eeeb8a34ff6a8a8f7e4
engelca <148014330+engelca@users.noreply.github.com>
2024-08-06 11:50:08 +1000

    Update common_mule_operator.py
    whitespace

--- src/common_mule_operator.py
+++ src/common_mule_operator.py
@@ -1,11 +1,10 @@
 import mule # pylint: disable=import-error
 
 class ReplaceOperator(mule.DataOperator):
-    
     """ Mule operator for replacing the data"""
     def __init__(self):
         pass
-        
+    
     def new_field(self, sources):
         print('new_field')
         return sources[0]
