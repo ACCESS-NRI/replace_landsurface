ee9ed02f276e2da95002236636c477682bffda45
Davide Marchegiani <davide.marchegiani@gmail.com>
2024-08-09 17:54:28 +1000

    Added test scripts for hres.py and replace_landsurface_with_era5land.py

---
+++ tests/test_hres.py
@@ -0,0 +1,16 @@
+import pytest
+from unittest.mock import patch
+
+# TODO: place ROSE DATA into a function (or an input argument) so it doesn't need to get called when importing the module
+import os
+os.environ['ROSE_DATA'] = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'test_data/rose_data')
+
+# TODO: Turn src into a package so that we can import the function directly
+# For now required to import from src
+import sys #To delete when src is a package
+srcpath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'src') #To delete when src is a package
+sys.path.insert(0,srcpath) #To delete when src is a package
+
+# from hres_ic import get_time, replace_input_file_with_tmp_input_file
+
+del sys.path[0] #To delete when src is a package
