b2973c9d84c48dc4d5e99883d0299b4499ee2eb0
Davide Marchegiani <davide.marchegiani@gmail.com>
2024-10-15 11:04:21 +1100

    Removed shebangs on scripts

--- src/replace_landsurface/hres_ic.py
+++ src/replace_landsurface/hres_ic.py
@@ -5,8 +5,6 @@
 #
 # Created by: Chermelle Engel <Chermelle.Engel@anu.edu.au>
 
-#!/usr/bin/env python3
-
 """Replace the land/surface fields in the astart file with higher-resolution
 era5-land or BARRA2-R data (if requested).
 """
@@ -18,58 +16,58 @@ from pathlib import Path
 import pandas
 
 from replace_landsurface import (
-    replace_landsurface_with_BARRA2R_IC,
-    replace_landsurface_with_ERA5land_IC,
-    replace_landsurface_with_FF_IC,
+	replace_landsurface_with_BARRA2R_IC,
+	replace_landsurface_with_ERA5land_IC,
+	replace_landsurface_with_FF_IC,
 )
 
 boolopt = {
-    "True": True,
-    "False": False,
+	"True": True,
+	"False": False,
 }
 
 
 def main():
-    """The main function that creates a worker pool and generates single GRIB files
-    for requested date/times in parallel.
+	"""The main function that creates a worker pool and generates single GRIB files
+	for requested date/times in parallel.
 
-    Parameters
-    ----------
-    None.  The arguments are given via the command-line
+	Parameters
+	----------
+	None.  The arguments are given via the command-line
 
-    Returns
-    -------
-    None.  The astart file is updated and overwritten
+	Returns
+	-------
+	None.  The astart file is updated and overwritten
 
-    """ 
-    # Parse the command-line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mask', required=True, type=Path)
-    parser.add_argument('--file', required=True, type=Path)
-    parser.add_argument('--start', required=True, type=pandas.to_datetime)
-    parser.add_argument('--type', default="era5land")
-    parser.add_argument('--hres_ic', type=Path)
-    args = parser.parse_args()
-    print(args)
+	"""
+	# Parse the command-line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--mask", required=True, type=Path)
+	parser.add_argument("--file", required=True, type=Path)
+	parser.add_argument("--start", required=True, type=pandas.to_datetime)
+	parser.add_argument("--type", default="era5land")
+	parser.add_argument("--hres_ic", type=Path)
+	args = parser.parse_args()
+	print(args)
 
-    # Convert the date/time to a formatted string
-    t = args.start.strftime("%Y%m%dT%H%MZ")
-    print(args.mask, args.file, t)
+	# Convert the date/time to a formatted string
+	t = args.start.strftime("%Y%m%dT%H%MZ")
+	print(args.mask, args.file, t)
 
-    # If necessary replace ERA5 land/surface fields with higher-resolution options
-    if "era5land" in args.type:
-        replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.mask, args.file, t)
-        shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
-    elif "barra" in args.type:
-        replace_landsurface_with_BARRA2R_IC.swap_land_barra(args.mask, args.file, t)
-        shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
-    elif "astart" in args.type:
-        replace_landsurface_with_FF_IC.swap_land_ff(args.mask, args.file, args.hres_ic,t)
-        shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
+	# If necessary replace ERA5 land/surface fields with higher-resolution options
+	if "era5land" in args.type:
+		replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.mask, args.file, t)
+		shutil.move(args.file.as_posix(), args.file.as_posix().replace(".tmp", ""))
+	elif "barra" in args.type:
+		replace_landsurface_with_BARRA2R_IC.swap_land_barra(args.mask, args.file, t)
+		shutil.move(args.file.as_posix(), args.file.as_posix().replace(".tmp", ""))
+	elif "astart" in args.type:
+		replace_landsurface_with_FF_IC.swap_land_ff(args.mask, args.file, args.hres_ic, t)
+		shutil.move(args.file.as_posix(), args.file.as_posix().replace(".tmp", ""))
 
-    else:
-        print("No need to swap out IC")
+	else:
+		print("No need to swap out IC")
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+	main()
