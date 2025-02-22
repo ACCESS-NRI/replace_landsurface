a84afbc090de75ddf9cc06e32d674dafd3d866c8
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-02-22 12:30:50 +1100

    Restructured hres_ic.py and replace_landsurface_with_BARRA2R_IC.py: - Simplified code removing redundant portions - Deleted redundant MASK input - Turned the class bounding_box to the function get_bounding_box, that returns the BoundingBox namedtuple. Simplified the way to get boundary box extents. - Restructured the get_barra_data function to use xarray DataArray.sel method, together with slice and 'method' keyword for more efficient and simpler data retrieval.

--- src/replace_landsurface/hres_ic.py
+++ src/replace_landsurface/hres_ic.py
@@ -9,12 +9,16 @@
 Replace the land/surface fields in the astart file with higher-resolution
 era5-land or BARRA2-R data (if requested).
 """
+#TODO: delete after testing
+# barra_fname='/g/data/vk83/testing/data/replace_landsurface/integration_tests/driving_data/etc/barra_r2/1hr/ts/latest/ts_AUS-11_ERA5_historical_hres_BOM_BARRA-R2_v1_1hr_202008-202008.nc'
+# fname='/g/data/tm70/cbe563/test_replace_land_surface/BARRAR2/GAL9_astart'
+# mask='/scratch/tm70/cbe563/cylc-run/u-dg767.b/share/data/ancils/Lismore/d1100/qrparm.mask'
+# date='202008090320'
+#TODO: delete after testing
 
 import argparse
 import shutil
 from pathlib import Path
-
-import pandas
 
 from replace_landsurface import (
     replace_landsurface_with_BARRA2R_IC,
@@ -39,29 +43,22 @@ def main():
 
     # Parse the command-line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mask', required=True, type=Path)
     parser.add_argument('--file', required=True, type=Path)
-    parser.add_argument('--start', required=True, type=pandas.to_datetime)
+    parser.add_argument('--start', required=True, type=str, help="A date in the format 'YYYYmmddHHMM'.")
     parser.add_argument('--type', default="era5land")
     parser.add_argument('--hres_ic', type=Path)
     args = parser.parse_args()
-    print(args)
-
-    # Convert the date/time to a formatted string
-    t = args.start.strftime("%Y%m%dT%H%MZ")
-    print(args.mask, args.file, t)
 
     # If necessary replace ERA5 land/surface fields with higher-resolution options
     if "era5land" in args.type:
-        replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.mask, args.file, t)
+        replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.file, args.start)
         shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
     elif "barra" in args.type:
-        replace_landsurface_with_BARRA2R_IC.swap_land_barra(args.mask, args.file, t)
+        replace_landsurface_with_BARRA2R_IC.swap_land_barra(args.file, args.start)
         shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
     elif "astart" in args.type:
-        replace_landsurface_with_FF_IC.swap_land_ff(args.mask, args.file, args.hres_ic,t)
+        replace_landsurface_with_FF_IC.swap_land_ff(args.file, args.hres_ic, args.start)
         shutil.move(args.file.as_posix(), args.file.as_posix().replace('.tmp', ''))
-
     else:
         print("No need to swap out IC")
 
