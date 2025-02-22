ac1f9c432a7da9c5a0cdc162f789bf75235a23e5
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-02-22 22:47:52 +1100

    - Removed dependency of replace_landsurface_with_FF_IC from --mask and other redundant variables - General minor updates

--- src/replace_landsurface/replace_landsurface_with_FF_IC.py
+++ src/replace_landsurface/replace_landsurface_with_FF_IC.py
@@ -12,63 +12,53 @@ class ReplaceOperator(mule.DataOperator):
     def __init__(self):
         pass
     def new_field(self, sources):
-        print('new_field')
         return sources[0]
     def transform(self, sources, result):
-        print('transform')
         return sources[1]
 
 def replace_in_ff_from_ff(f, sf, mf_out, replace):
-
     replacement_data = sf.get_data()
     mf_out.fields.append(replace([f, replacement_data]))
 
-def swap_land_ff(mask_fullpath, ic_file_fullpath, source_fullpath, ic_date):
+def swap_land_ff(ic_file_fullpath, source_fullpath):
     """
     Function to get the land/surface data from another fields file into the start dump.
 
     Parameters
     ----------
-    mask_fullpath : Path
-        Path to the mask defining the spatial extent
     ic_file_fullpath : Path
         Path to file with the coarser resolution data to be replaced with ".tmp" appended at end
     source_fullpath : Path
         Path to source fields file to take the land/surface data from
-    ic_date : string
-        The date-time required in "%Y%m%d%H%M" format
 
     Returns
     -------
     None.
         The file is replaced with a version of itself holding the higher-resolution data.
     """
-
-    print(mask_fullpath, ic_file_fullpath, source_fullpath,ic_date)
    
     # Path to input file 
     ff_in = ic_file_fullpath.as_posix().replace('.tmp', '')
 
-    # Path to input file 
-    sf_in = source_fullpath.as_posix()
-
     # Path to output file 
     ff_out = ic_file_fullpath.as_posix()
-    print(ff_in, ff_out)
-   
+
+    # Path to source input file 
+    sf_in = source_fullpath.as_posix()
+
     # Read input file
     mf_in = mule.load_umfile(ff_in)
+    # Read source file
     msf_in = mule.load_umfile(sf_in)
    
+    # Set up the output file
+    mf_out = mf_in.copy()
+    
     # Create Mule Replacement Operator
     replace = ReplaceOperator() 
 
-    # Set up the output file
-    mf_out = mf_in.copy()
-
     # For each field in the input write to the output file (but modify as required)
     for f,sf in zip(mf_in.fields,msf_in.fields):
-    
         if f.lbuser4 in [9, 20, 24]:
             replace_in_ff_from_ff(f, sf, mf_out, replace)
         else:
