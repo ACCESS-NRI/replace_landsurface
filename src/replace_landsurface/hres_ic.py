e432c6a90ce3ef6ead6050df7dfd4d475327bf0a
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-02-23 15:50:19 +1100

    Addedd replace_landsurface.py to be used for both 'barra' and 'era5land' types

--- src/replace_landsurface/hres_ic.py
+++ src/replace_landsurface/hres_ic.py
@@ -12,12 +12,7 @@ era5-land or BARRA2-R data (if requested).
 
 import argparse
 from pathlib import Path
-
-from replace_landsurface import (
-    replace_landsurface_with_BARRA2R_IC,
-    replace_landsurface_with_ERA5land_IC,
-    replace_landsurface_with_FF_IC,
-)
+from replace_landsurface import replace_landsurface, replace_landsurface_with_FF_IC
 
 def main():
 
@@ -43,14 +38,12 @@ def main():
     args = parser.parse_args()
 
     # If necessary replace ERA5 land/surface fields with higher-resolution options
-    if "era5land" in args.type:
-        replace_landsurface_with_ERA5land_IC.swap_land_era5land(args.file, args.start)
-    elif "barra" in args.type:
-        replace_landsurface_with_BARRA2R_IC.swap_land_barra(args.file, args.start)
-    elif "astart" in args.type:
+    if args.type in ("era5land", "barra"):
+        replace_landsurface.swap_land(args.file, args.start, args.type)
+    elif args.type == "astart":
         replace_landsurface_with_FF_IC.swap_land_ff(args.file, args.hres_ic)
     else:
-        print("No need to swap out IC")
+        raise ValueError(f"Unsupported '--type' argument: {args.type}")
 
 if __name__ == '__main__':
     main()
---
+++ src/replace_landsurface/replace_landsurface.py
@@ -0,0 +1,347 @@
+# Copyright 2024 ACCESS-NRI (https://www.access-nri.org.au/)
+# See the top-level COPYRIGHT.txt file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Initially created by: Chermelle Engel <Chermelle.Engel@anu.edu.au>
+
+import os
+import warnings
+from glob import glob
+from collections import namedtuple
+from pathlib import Path
+
+import mule
+import numpy as np
+import xarray as xr
+
+TEMPORARY_SUFFIX = ".tmp"
+DECIMAL_PRECISION = 4
+ROSE_DATA = os.environ.get("ROSE_DATA", "")
+
+STASH_SOIL_MOISTURE = 9
+STASH_SOIL_TEMPERATURE = 20
+STASH_SURFACE_TEMPERATURE = 24
+STASH_LAND_MASK = 30
+
+BoundingBox = namedtuple("BoundingBox", ("latmin", "latmax", "lonmin", "lonmax"))
+
+
+class ReplaceOperator(mule.DataOperator):
+    """Mule operator for replacing the data of a field"""
+
+    def __init__(self) -> None:
+        pass
+
+    def new_field(self, source_field: mule.Field) -> mule.Field:
+        return source_field.copy()
+
+    def transform(self, source_field, newdata: np.ndarray) -> np.ndarray:
+        return newdata
+
+
+def get_bounding_box(mule_file: mule.UMFile) -> BoundingBox:
+    """Get spatial extent information for the replacement.
+
+    Parameters
+    ----------
+    mule_file : mule.UMFile object
+        Input UM fields file to be replaced
+
+    Returns
+    -------
+    BoundingBox
+        The BoundingBox namedtuple containing the spatial extent of the input file
+    """
+    # Get extent from the land mask (stash 30) if present, otherwise use the first field
+    # We don't use the fields file extent because sometimes it is not correct
+    try:
+        field = [f for f in mule_file.fields if f.lbuser4 == STASH_LAND_MASK][0]
+    except IndexError:
+        field = mule_file.fields[0]
+    # Get the latitude extent
+    dlat = field.bdy
+    nlat = mule_file.integer_constants.num_rows
+    latmin = round(
+        field.bzy + dlat, DECIMAL_PRECISION
+    )  # rounded to avoid numerical inaccuracy
+    latmax = round(
+        latmin + (nlat - 1) * dlat, DECIMAL_PRECISION
+    )  # rounded to avoid numerical inaccuracy
+
+    # Get the latitude extent
+    dlon = field.bdx
+    nlon = mule_file.integer_constants.num_cols
+    lonmin = round(
+        field.bzx + dlon, DECIMAL_PRECISION
+    )  # rounded to avoid numerical inaccuracy
+    lonmax = round(
+        lonmin + (nlon - 1) * dlon, DECIMAL_PRECISION
+    )  # rounded to avoid numerical inaccuracy
+
+    return BoundingBox(latmin, latmax, lonmin, lonmax)
+
+
+def get_latitude_name(file: xr.Dataset | xr.DataArray) -> str:
+    """
+    Get the name of the latitude coordinate in the file ('lat' or 'latitude').
+
+    Parameters
+    ----------
+    file : xarray.Dataset or xarray.DataArray
+        The xarray Dataset or DataArray to get the latitude name from.
+
+    Returns
+    -------
+    string
+        The name of the latitude coordinate.
+    """
+    if "latitude" in file.coords:
+        return "latitude"
+    elif "lat" in file.coords:
+        return "lat"
+    else:
+        raise ValueError("The latitude coordinate name could not be understood.")
+
+
+def get_longitude_name(file: xr.Dataset | xr.DataArray) -> str:
+    """
+    Get the name of the longitude coordinate in the file ('lon' or 'longitude').
+
+    Parameters
+    ----------
+    file : xarray.Dataset or xarray.DataArray
+        The xarray Dataset or DataArray to get the longitude name from.
+
+    Returns
+    -------
+    string
+        The name of the longitude coordinate.
+    """
+    if "longitude" in file.coords:
+        return "longitude"
+    elif "lon" in file.coords:
+        return "lon"
+    else:
+        raise ValueError("The longitude coordinate name could not be understood.")
+
+
+def get_hres_data(
+    fname: str,
+    var_name: str,
+    date: str,
+    bounds: BoundingBox,
+) -> np.ndarray:
+    """
+    Get the high-resolution data from the data_source file.
+    based on the var_name variable.
+
+    Parameters
+    ----------
+    fname : string
+        The name of the high-resolution file to get data from.
+    var_name : string
+        The variable name to get data from.
+    date : string
+        The date in the format "YYYYmmddHHMM".
+    bounds : BoundingBox
+        BoundingBox namedtuple containing the spatial extent to retrieve the data.
+
+    Returns
+    -------
+    numpy.ndarray
+        A numpy array containg the data for the date/time and spatial extent
+    """
+
+    # Open the file containing the data
+    if Path(fname).exists():
+        data = xr.open_dataset(fname)
+    else:
+        raise FileNotFoundError(f"File '{fname}' not found.")
+    # Select the variable
+    try:
+        data = data[var_name]
+    except KeyError:
+        raise ValueError(
+            f"Variable '{var_name}' not found in high-resolution file '{fname}'."
+        )
+
+    latitude_name = get_latitude_name(data)
+    longitude_name = get_longitude_name(data)
+
+    # If needed, modify data so it has the same structure as UM files: ascending latitudes (-90 to 90) and positive longitudes (0 to 360)
+    # Guarantee ascending latitude
+    if data[latitude_name][0] > data[latitude_name][1]:  # descending latitude
+        data = data.reindex(
+            {latitude_name: data[latitude_name][::-1]}
+        )  # flip the latitude
+
+    # Guarantee positive longitudes
+    if any(data[longitude_name] < 0):  # negative longitude (-180 to 180)
+        longitude = data[longitude_name]
+        new_index = [lon for lon in longitude if lon >= 0] + [
+            lon for lon in longitude if lon < 0
+        ]  # move negative longitudes to the end
+        data = data.reindex({longitude_name: new_index})
+        new_longitude = [
+            lon if lon >= 0 else (360 + lon) for lon in new_index
+        ]  # turn longitudes into (0 to 360) structure
+        data = data.assign_coords({longitude_name: new_longitude})
+
+    # Select the date
+    try:
+        data = data.sel(time=date)
+    except KeyError:
+        # If the exact date is not found select the nearest date and send a warning message
+        data = data.sel(time=date, method="nearest")
+        actual_date = (
+            data.time.values.astype("datetime64[m]").tolist().strftime("%Y%m%d%H%M")
+        )
+        warnings.warn(
+            f"The date '{date}' was not found in the high-resolution file '{fname}'.\n"
+            f"Using the nearest date '{actual_date}' instead."
+        )
+
+    # Select the spatial extent
+    data = data.sel({
+        latitude_name: slice(bounds.latmin, bounds.latmax),
+        longitude_name: slice(bounds.lonmin, bounds.lonmax),
+    })
+    return data.data
+
+def get_new_data(
+    field: mule.Field,
+    fname: str,
+    var_name: str,
+    date: str,
+    bounds: BoundingBox,
+    multiplier: float = 1,
+) -> np.ndarray:
+    """
+    Get the new high-resolution data for the replacement.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    new_data : numpy.ndarray
+        A numpy array containing the new data.
+    """
+    current_data = field.get_data()
+    data = get_hres_data(fname, var_name, date, bounds)
+    data = data * multiplier
+    # If data is missing, keep the original data
+    new_data = np.where(np.isnan(data), current_data, data)
+    return new_data
+
+def get_era5land_fname(var_name: str, date: str):
+    ERA_DIR = os.path.join(ROSE_DATA, "etc", "era5_land")
+    year = date[0:4]
+    month = date[4:6]
+    indir = os.path.join(ERA_DIR, var_name, year)
+    try:
+        fname = glob(os.path.join(indir, f'*_{year}{month}??-{year}{month}??.nc'))[0]
+    except IndexError:
+        raise FileNotFoundError(f"No ERA5-Land file found in '{indir}' for date '{date}'.")
+    return fname
+
+def swap_land_era5land(
+    mf_in: mule.UMFile,
+    date: str,
+    replace_operator: ReplaceOperator,
+    bounds: BoundingBox,
+) -> mule.UMFile:
+    """
+    Replace the input file with the high-resolution era5-land data.
+
+    Parameters
+    ----------
+    mf_in : mule.UMFile
+        The input file with the coarser resolution data to be replaced with the high-resolution one.
+    date : string
+        The requested date for the high-resolution data, in the format "YYYYmmddHHMM".
+    replace_operator : ReplaceOperator
+        The Mule operator for replacing the data.
+    bounds : BoundingBox
+        The spatial extent for the replacement.
+    
+    Returns
+    -------
+    mf_out : mule.UMFile.
+        The output mule UMFile containing the updated data.
+    """
+    # The depths of soil for the conversion
+    SOIL_MULTIPLIERS = (100.0, 250.0, 650.0, 2000.0)
+    # Base directory of the ERA5-land archive on NCI
+
+    mf_out = mf_in.copy(include_fields=True)
+    # Find one "swvl1" file in the archive and create a generic filename
+   
+    # For each field in the input write to the output file (but modify as required)
+    for indf,field in enumerate(mf_in.fields):
+        lblev = field.lblev
+        stash = field.lbuser4
+        if stash in (STASH_SOIL_MOISTURE, STASH_SOIL_TEMPERATURE, STASH_SURFACE_TEMPERATURE):
+            if field.lbuser4 == STASH_SOIL_MOISTURE:
+                var_name = "swvl" + str(lblev)
+                multiplier = SOIL_MULTIPLIERS[lblev - 1]
+            elif field.lbuser4 == STASH_SOIL_TEMPERATURE:
+                var_name = "stl" + str(lblev)
+                multiplier = 1
+            elif field.lbuser4 == STASH_SURFACE_TEMPERATURE:
+                var_name = "skt"
+                multiplier = 1
+            fname = get_era5land_fname(var_name, date)
+            new_data = get_new_data(field, fname, var_name, date, bounds, multiplier)
+            mf_out.fields[indf] = replace_operator(field, new_data)
+    return mf_out
+   
+
+def swap_land(fname: Path, date: str, source_data_type: str) -> None:
+    """
+    Replace the input file with the high-resolution data.
+
+    Parameters
+    ----------
+    fname : string
+        Path to the input file with the coarser resolution data to be replaced with the high-resolution one.
+    date : string
+        The requested date for the high-resolution data, in the format "YYYYmmddHHMM".
+    source_data_type : string
+        The source data type. Can be one among: ('era5land', 'barra').
+
+    Returns
+    -------
+    None.
+        A new file is written, overwriting the input fname.
+    """
+    
+    # Input filepath
+    ff_in = fname.as_posix()
+    # Output temporary filepath
+    ff_out_tmp = ff_in + TEMPORARY_SUFFIX
+    
+    # Read the input file
+    mf_in = mule.load_umfile(ff_in)
+    
+    # Get the spatial extent for the replacement
+    bounds = get_bounding_box(mf_in)
+
+    # Create a Mule Replacement Operator
+    replace_op = ReplaceOperator()
+
+    if source_data_type == "era5land":
+        mf_out = swap_land_era5land(mf_in, date, replace_op, bounds)
+    elif source_data_type == "barra":
+        mf_out = swap_land_barra()
+    else:
+        raise ValueError(f"Unsupported source_data_type: {source_data_type}")
+
+    # Write output file
+    mf_out.validate = lambda *args, **kwargs: True
+    mf_out.to_file(ff_out_tmp)
+    # Replace original input file with temporary output file
+    os.rename(ff_out_tmp, ff_in)
+    
